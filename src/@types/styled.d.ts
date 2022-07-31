c8a3b725d3c4ca8d30cf372b2ae0b86d750832bd
Gabriel Lucas <gabriellcontatoprofissional@gmail.com>
2022-07-31 13:47:00 -0300

    adding router dom

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,14 +1,7 @@
-import styled from "styled-components";
-import { defaultTheme } from "../styles/themes/default";
+import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
-// criando tipagem para o módulo styled components
-// sempre que for importado styled components, a definição de tipos que ele irá puxar,
-// será a for definida aqui.
-
-// se não estivesse importado o styled components, você estaria criando uma nova tipagem
-// para o módulo. A ideia aqui será sobrescrever.
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
